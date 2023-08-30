7fcd027ee31374007f3d9ac3389c61d7cf327f14
Harsh Sahu <harshsahu.8085@gmail.com>
2023-08-30 19:37:47 +0200

    FTIR & OTICE date time filter

--- OTICE_script.R
+++ OTICE_script.R
@@ -16,30 +16,31 @@ library(data.table)
 source("https://raw.githubusercontent.com/harshest-human/GCDP/main/GCDP_function_script.R")
 
 ########### OTICE DATA IMPORT ###############
-ODP(raw_path="D:/Data Analysis/Gas_data/Raw_data/OTICE_raw",
-    clean_path="D:/Data Analysis/Gas_data/Clean_data/OTICE_clean")
-
+ODP(raw_path="D:/Data Analysis/Gas_data/Raw_data/OTICE_raw", clean_path="D:/Data Analysis/Gas_data/Clean_data/OTICE_clean")
 OTICE_data <- read.csv("D:/Data Analysis/Gas_data/Clean_data/OTICE_clean/20230830_ODP.CSV")
 
-
 ########### FTIR DATA IMPORT ###############
-FDP(raw_path="D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/2023-07-08_FTIR.TXT",
-    clean_path="D:/Data Analysis/Gas_data/Clean_data/FTIR_clean")
+FDP(raw_path="D:/Data Analysis/Gas_data/Raw_data/FTIR_raw/2023-07-08_FTIR.TXT", clean_path="D:/Data Analysis/Gas_data/Clean_data/FTIR_clean")
+FTIR_data <- read.csv("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/20230830_FDP.CSV") #check today's date
 
-FTIR_data <- read.csv("D:/Data Analysis/Gas_data/Clean_data/FTIR_clean/20230830_FDP.CSV")
+########### MERGE OTICE & FTIR DATA (Time series) ###############
+FTIR_data <- FTIR_data %>% 
+  filter(Date.time >= "2023-07-21 13:20:38",
+         Date.time <= "2023-07-21 13:48:00") %>% na.omit()
+write.csv(FTIR_1, file = "FTIR_1.csv",row.names = FALSE)
+
+OTICE_data <- OTICE_data %>% 
+  filter(Date.time >= "2023-07-21 13:20:38",
+         Date.time <= "2023-07-21 13:48:00") %>% na.omit()
+write.csv(OTICE_1, file = "OTICE_1.csv",row.names = FALSE)
+
+# Merge data by rounding to nearest timestamp
+merged_data <- FTIR_data[, .(Date.time, Sampling.point.F, CO2.F, NH3.F, CH4.F, H2O.F)]
+merged_data <- OTICE_data[merged_data, on = "Date.time", roll = "nearest"]
 
 ########### DATA VISUALIZATION ###############
-FTIR_1 <- FTIR_data %>% 
-  filter(Date.time.F >= "2023-07-21 13:20:38",
-         Date.time.F <= "2023-07-21 13:48:00") %>% na.omit()
 
 ggline(FTIR_1, x="Date.time.F", y="CH4.F")
-
-
-#OTICE
-OTICE_1 <- OTICE_data %>% 
-  filter(Date.time.O >= "2023-07-21 13:20:38",
-         Date.time.O <= "2023-07-21 13:48:00") %>% na.omit()
 
 ggline(OTICE_1, x="Date.time.O", y="CH4.O")
 
