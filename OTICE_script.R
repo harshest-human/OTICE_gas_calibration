f797efa751ed6c1241b9fbab3613042942f88853
Harsh Sahu <harshsahu.8085@gmail.com>
2023-09-05 21:57:31 +0200

    sec.axis=FTIR, sec.axis=Rs/Ro

--- OTICE_script.R
+++ OTICE_script.R
@@ -53,7 +53,9 @@ OTICE_1 <- OTICE_data %>%
   summarise(
     mean_temperature = mean(temperature),
     CH4.O = mean(CH4.O),
-    NH3.O = mean(NH3),
+    CH4 = mean(CH4),
+    NH3.O = mean(NH3.O),
+    NH3 = mean(NH3),
     CO2 = mean(CO2))
 
 OTICE_2 <- OTICE_data %>% 
@@ -64,7 +66,9 @@ OTICE_2 <- OTICE_data %>%
   summarise(
     mean_temperature = mean(temperature),
     CH4.O = mean(CH4.O),
-    NH3.O = mean(NH3),
+    CH4 = mean(CH4),
+    NH3.O = mean(NH3.O),
+    NH3 = mean(NH3),
     CO2 = mean(CO2))
 
 OTICE_3 <- OTICE_data %>% 
@@ -75,7 +79,9 @@ OTICE_3 <- OTICE_data %>%
   summarise(
     mean_temperature = mean(temperature),
     CH4.O = mean(CH4.O),
-    NH3.O = mean(NH3),
+    CH4 = mean(CH4),
+    NH3.O = mean(NH3.O),
+    NH3 = mean(NH3),
     CO2 = mean(CO2))
 
 OTICE_4 <- OTICE_data %>%
@@ -86,7 +92,9 @@ OTICE_4 <- OTICE_data %>%
   summarise(
     mean_temperature = mean(temperature),
     CH4.O = mean(CH4.O),
-    NH3.O = mean(NH3),
+    CH4 = mean(CH4),
+    NH3.O = mean(NH3.O),
+    NH3 = mean(NH3),
     CO2 = mean(CO2))
 
 OTICE_5 <- OTICE_data %>% 
@@ -97,7 +105,9 @@ OTICE_5 <- OTICE_data %>%
   summarise(
     mean_temperature = mean(temperature),
     CH4.O = mean(CH4.O),
-    NH3.O = mean(NH3),
+    CH4 = mean(CH4),
+    NH3.O = mean(NH3.O),
+    NH3 = mean(NH3),
     CO2 = mean(CO2))
 
 OTICE_6 <- OTICE_data %>% 
@@ -108,7 +118,9 @@ OTICE_6 <- OTICE_data %>%
   summarise(
     mean_temperature = mean(temperature),
     CH4.O = mean(CH4.O),
-    NH3.O = mean(NH3),
+    CH4 = mean(CH4),
+    NH3.O = mean(NH3.O),
+    NH3 = mean(NH3),
     CO2 = mean(CO2))
 
 OTICE_7 <- OTICE_data %>% 
@@ -119,7 +131,9 @@ OTICE_7 <- OTICE_data %>%
   summarise(
     mean_temperature = mean(temperature),
     CH4.O = mean(CH4.O),
-    NH3.O = mean(NH3),
+    CH4 = mean(CH4),
+    NH3.O = mean(NH3.O),
+    NH3 = mean(NH3),
     CO2 = mean(CO2))
 
 OTICE_8 <- OTICE_data %>% 
@@ -130,7 +144,9 @@ OTICE_8 <- OTICE_data %>%
   summarise(
     mean_temperature = mean(temperature),
     CH4.O = mean(CH4.O),
-    NH3.O = mean(NH3),
+    CH4 = mean(CH4),
+    NH3.O = mean(NH3.O),
+    NH3 = mean(NH3),
     CO2 = mean(CO2))
 
 OTICE_9 <- OTICE_data %>% 
@@ -141,7 +157,9 @@ OTICE_9 <- OTICE_data %>%
   summarise(
     mean_temperature = mean(temperature),
     CH4.O = mean(CH4.O),
-    NH3.O = mean(NH3),
+    CH4 = mean(CH4),
+    NH3.O = mean(NH3.O),
+    NH3 = mean(NH3),
     CO2 = mean(CO2))
 
 # FTIR
@@ -190,20 +208,20 @@ FTIR_8 <- FTIR_data %>%
 
 ########### MERGE ###############
 # Merge FTIR and OTICE
-merge_4 <- left_join(OTICE_4, FTIR_4, by = c("Date.time" = "Date.time"))
-merge_4 <- select(merge_4 , Date.time, Sampling.point, NH3.O, NH3.F, CH4.O, CH4.F, CO2.F, CO2)
+merge_4 <- left_join(OTICE_4, FTIR_4, by = c("Date.time" = "Date.time")) %>% na.omit()
+merge_4 <- select(merge_4 , Date.time, Sampling.point, NH3.O, NH3.F, NH3, CH4.O, CH4.F, CH4, CO2.F, CO2)
 
-merge_5 <- left_join(OTICE_5, FTIR_5, by = c("Date.time" = "Date.time"))
-merge_5 <- select(merge_5 , Date.time, Sampling.point, NH3.O, NH3.F, CH4.O, CH4.F, CO2.F, CO2)
+merge_5 <- left_join(OTICE_5, FTIR_5, by = c("Date.time" = "Date.time")) %>% na.omit()
+merge_5 <- select(merge_5 , Date.time, Sampling.point, NH3.O, NH3.F, NH3, CH4.O, CH4.F, CH4, CO2.F, CO2)
 
-merge_6 <- left_join(OTICE_6, FTIR_6, by = c("Date.time" = "Date.time"))
-merge_6 <- select(merge_6 , Date.time, Sampling.point, NH3.O, NH3.F, CH4.O, CH4.F, CO2.F, CO2)
+merge_6 <- left_join(OTICE_6, FTIR_6, by = c("Date.time" = "Date.time")) %>% na.omit()
+merge_6 <- select(merge_6 , Date.time, Sampling.point, NH3.O, NH3.F, NH3, CH4.O, CH4.F, CH4, CO2.F, CO2)
 
-merge_7 <- left_join(OTICE_7, FTIR_7, by = c("Date.time" = "Date.time"))
-merge_7 <- select(merge_7 , Date.time, Sampling.point, NH3.O, NH3.F, CH4.O, CH4.F, CO2.F, CO2)
+merge_7 <- left_join(OTICE_7, FTIR_7, by = c("Date.time" = "Date.time")) %>% na.omit()
+merge_7 <- select(merge_7 , Date.time, Sampling.point, NH3.O, NH3.F, NH3, CH4.O, CH4.F, CH4, CO2.F, CO2)
 
-merge_8 <- left_join(OTICE_8, FTIR_8, by = c("Date.time" = "Date.time"))
-merge_8 <- select(merge_8 , Date.time, Sampling.point, NH3.O, NH3.F, CH4.O, CH4.F, CO2.F, CO2)
+merge_8 <- left_join(OTICE_8, FTIR_8, by = c("Date.time" = "Date.time")) %>% na.omit()
+merge_8 <- select(merge_8 , Date.time, Sampling.point, NH3.O, NH3.F, NH3, CH4.O, CH4.F, CH4, CO2.F, CO2)
 
 
 ########### CH4 VIZ ###############
@@ -215,6 +233,7 @@ CH4_plot4 <- ggplot(merge_4, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "CH4 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -228,6 +247,7 @@ CH4_plot5 <- ggplot(merge_5, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "CH4 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -241,6 +261,7 @@ CH4_plot6 <- ggplot(merge_6, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "CH4 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -254,6 +275,7 @@ CH4_plot7 <- ggplot(merge_7, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "CH4 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -267,6 +289,7 @@ CH4_plot8 <- ggplot(merge_8, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "CH4 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -280,6 +303,7 @@ NH3_plot4 <- ggplot(merge_4, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "NH3 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -293,6 +317,7 @@ NH3_plot5 <- ggplot(merge_5, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "NH3 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -306,6 +331,7 @@ NH3_plot6 <- ggplot(merge_6, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "NH3 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -319,6 +345,7 @@ NH3_plot7 <- ggplot(merge_7, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "NH3 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -331,6 +358,7 @@ NH3_plot8 <- ggplot(merge_8, aes(x = Date.time)) +
   labs(x = "Date and Time",
        y = "NH3 PPM",
        color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "FTIR")) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "right") +
   ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
@@ -362,3 +390,185 @@ ggsave("NH3_plot5.pdf", NH3_plot5, width = 8, height = 5, units = "in")
 ggsave("NH3_plot6.pdf", NH3_plot6, width = 8, height = 5, units = "in")
 ggsave("NH3_plot7.pdf", NH3_plot7, width = 8, height = 5, units = "in")
 ggsave("NH3_plot8.pdf", NH3_plot8, width = 8, height = 5, units = "in")
+
+
+###### Rs/Ro Viz ######
+CH4_plot4 <- ggplot(merge_4, aes(x = Date.time)) +
+  geom_line(aes(y = CH4.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = CH4.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = CH4, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_4$Date.time), max(merge_4$Date.time), by = "5 mins")) +
+  labs(x = "Date and Time",
+       y = "CH4 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+
+CH4_plot5 <- ggplot(merge_5, aes(x = Date.time)) +
+  geom_line(aes(y = CH4.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = CH4.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = CH4, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_5$Date.time), max(merge_5$Date.time), by = "1 hour")) +
+  labs(x = "Date and Time",
+       y = "CH4 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+
+CH4_plot6 <- ggplot(merge_6, aes(x = Date.time)) +
+  geom_line(aes(y = CH4.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = CH4.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = CH4, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_6$Date.time), max(merge_6$Date.time), by = "6 hours")) +
+  labs(x = "Date and Time",
+       y = "CH4 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+
+CH4_plot7 <- ggplot(merge_7, aes(x = Date.time)) +
+  geom_line(aes(y = CH4.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = CH4.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = CH4, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_7$Date.time), max(merge_7$Date.time), by = "12 hours")) +
+  labs(x = "Date and Time",
+       y = "CH4 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+
+CH4_plot8 <- ggplot(merge_8, aes(x = Date.time)) +
+  geom_line(aes(y = CH4.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = CH4.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = CH4, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_8$Date.time), max(merge_8$Date.time), by = "6 hours")) +
+  labs(x = "Date and Time",
+       y = "CH4 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+
+# NH3
+NH3_plot4 <- ggplot(merge_4, aes(x = Date.time)) +
+  geom_line(aes(y = NH3.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = NH3.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = NH3, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_4$Date.time), max(merge_4$Date.time), by = "5 mins")) +
+  labs(x = "Date and Time",
+       y = "NH3 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+
+NH3_plot5 <- ggplot(merge_5, aes(x = Date.time)) +
+  geom_line(aes(y = NH3.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = NH3.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = NH3, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_5$Date.time), max(merge_5$Date.time), by = "1 hour")) +
+  labs(x = "Date and Time",
+       y = "NH3 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+
+NH3_plot6 <- ggplot(merge_6, aes(x = Date.time)) +
+  geom_line(aes(y = NH3.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = NH3.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = NH3, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_6$Date.time), max(merge_6$Date.time), by = "6 hours")) +
+  labs(x = "Date and Time",
+       y = "NH3 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+
+NH3_plot7 <- ggplot(merge_7, aes(x = Date.time)) +
+  geom_line(aes(y = NH3.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = NH3.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = NH3, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_7$Date.time), max(merge_7$Date.time), by = "12 hours")) +
+  labs(x = "Date and Time",
+       y = "NH3 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+
+NH3_plot8 <- ggplot(merge_8, aes(x = Date.time)) +
+  geom_line(aes(y = NH3.O, color = Sampling.point), size = 1) +
+  geom_line(aes(y = NH3.F, color = "FTIR"), size = 1, color = "black") +
+  geom_line(aes(y = NH3, color = Sampling.point), size = 1, linetype = "dashed") +
+  scale_x_datetime(date_labels = "%Y-%m-%d %H:%M", 
+                   breaks = seq(min(merge_8$Date.time), max(merge_8$Date.time), by = "6 hours")) +
+  labs(x = "Date and Time",
+       y = "NH3 PPM",
+       color = "Sensor Nodes") +
+  scale_y_continuous(sec.axis = sec_axis(~ ., name = "Rs/Ro", breaks = NULL)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "right") +
+  ggtitle("Comparing OTICE nodes with a reference FTIR inside a calibration chamber")
+
+###### View plots ######
+CH4_plot4
+CH4_plot5
+CH4_plot6
+CH4_plot7
+CH4_plot8
+
+NH3_plot4
+NH3_plot5
+NH3_plot6
+NH3_plot7
+NH3_plot8
+
+###### Save plots as a PDF ######
+# Save each plot on a separate page in the PDF
+ggsave("CH4_plot4_R.pdf", CH4_plot4, width = 8, height = 5, units = "in")
+ggsave("CH4_plot5_R.pdf", CH4_plot5, width = 8, height = 5, units = "in")
+ggsave("CH4_plot6_R.pdf", CH4_plot6, width = 8, height = 5, units = "in")
+ggsave("CH4_plot7_R.pdf", CH4_plot7, width = 8, height = 5, units = "in")
+ggsave("CH4_plot8_R.pdf", CH4_plot8, width = 8, height = 5, units = "in")
+
+ggsave("NH3_plot4_R.pdf", NH3_plot4, width = 8, height = 5, units = "in")
+ggsave("NH3_plot5_R.pdf", NH3_plot5, width = 8, height = 5, units = "in")
+ggsave("NH3_plot6_R.pdf", NH3_plot6, width = 8, height = 5, units = "in")
+ggsave("NH3_plot7_R.pdf", NH3_plot7, width = 8, height = 5, units = "in")
+ggsave("NH3_plot8_R.pdf", NH3_plot8, width = 8, height = 5, units = "in")
+
+
+
